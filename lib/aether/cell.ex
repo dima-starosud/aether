b06e2328df2685738b3d7d88ad2e265ad94dfe14
Dmytro Starosud <d.starosud@gmail.com>
2015-11-22 23:16:30 +0200

    minor renaming

--- lib/aether/cell.ex
+++ lib/aether/cell.ex
@@ -13,7 +13,7 @@ defmodule Aether.Cell do
 		me = self()
 		{^me, _} = :gproc.reg_or_locate({:n, :l, id})
 		state = %Cell{id: id, handler: wrap_handler(handler)}
-		radiate(id, waves)
+		schedule_radiation(id, waves)
 		{:ok, state}
 	end
 
@@ -24,16 +24,20 @@ defmodule Aether.Cell do
 	defp wrap_handler({m, f, a}), do: &apply(m, f, a ++ [&1, &2])
 	defp wrap_handler(f) when is_function(f, 2), do: f
 
-	defp do_radiate(from, to, wave) do
+	defp radiate_wave(from, to, wave) do
 		for pid <- :gproc.lookup_pids({:n, :l, to}) do
 			GenServer.cast(pid, {:wave, from, wave})
 		end
+		:ok
 	end
 
-	defp radiate(from, waves) do
-		for wave <- waves do
-			:timer.apply_after(wave.after, :erlang, :apply, [&do_radiate/3, [from, wave.to, wave.wave]])
+	defp schedule_radiation(from, radiations) do
+		for r <- radiations do
+			:timer.apply_after(r.after || 0, 
+												 :erlang, :apply,
+												 [&radiate_wave/3, [from, r.to, r.wave]])
 		end
+		:ok
 	end
 
 	def handle_call(_, _from, state) do
@@ -41,9 +45,9 @@ defmodule Aether.Cell do
 	end
 
 	def handle_cast({:wave, from, wave}, state) do
-		{handler, waves} = state.handler.(from, wave)
+		{handler, radiations} = state.handler.(from, wave)
 		state = %Cell{state | handler: wrap_handler(handler || state.handler)}
-		radiate(state.id, waves)
+		schedule_radiation(state.id, radiations)
 		{:noreply, state}
 	end
 
