86162c99b0907a2188eb7f26b031f75b236d84be
Dmytro Starosud <d.starosud@gmail.com>
2015-11-24 21:51:30 +0200

    kill cell after test

--- test/aether_test/cell_test.exs
+++ test/aether_test/cell_test.exs
@@ -7,14 +7,16 @@ defmodule AetherTest.CellTest do
 
   test "cell sends initial waves to handler" do
 		expected = %Aether.Wave{meta: :test, data: self()}
-		{:ok, _pid} = Aether.Cell.start_link(:unique_id, redirect(self()), [expected])
+		{:ok, pid} = Aether.Cell.start_link(:unique_id, redirect(self()), [expected])
+		on_exit killer(pid)
 		assert_receive ^expected
   end
 
 	test "cell broadcasts handler output waves" do
 		[expected1, expected2] = Enum.map([1, 2], &(%Aether.Wave{meta: :test, data: {self(), &1}}))
 		radiate = %Aether.Cell.Radiate{to: :unique_id, wave: expected2}
-		{:ok, _pid} = Aether.Cell.start_link(:unique_id, redirect(self(), [radiate]), [expected1])
+		{:ok, pid} = Aether.Cell.start_link(:unique_id, redirect(self(), [radiate]), [expected1])
+		on_exit killer(pid)
 		assert_receive ^expected1
 		assert_receive ^expected2
 	end
@@ -23,6 +25,13 @@ defmodule AetherTest.CellTest do
 		fn _from, wave ->
 			send(pid, wave)
 			{nil, mock}
+		end
+	end
+
+	def killer(pid) do
+		fn ->
+			:erlang.exit(pid, :kill)
+			IO.inspect {:killed, pid}
 		end
 	end
 end
