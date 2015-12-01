92282425e3362e510d1193056b7da4c539d97fcf
Dmytro Starosud <d.starosud@gmail.com>
2015-12-01 21:02:17 +0200

    minor changes

--- lib/aether/cell.ex
+++ lib/aether/cell.ex
@@ -1,17 +1,17 @@
 defmodule Aether.Cell do
-	use GenServer
-	require Logger
+  use GenServer
+  require Logger
 
-	alias __MODULE__
-	defstruct [:id, :handler]
+  alias __MODULE__
+  defstruct [:id, :handler]
 
-	def start(id, handler, radiations \\ []) do
-		GenServer.start(Cell, [id, handler, radiations])
-	end
+  def start(id, handler, radiations \\ []) do
+    GenServer.start(Cell, [id, handler, radiations])
+  end
 
-	def start_link(id, handler, radiations \\ []) do
-		GenServer.start_link(Cell, [id, handler, radiations])
-	end
+  def start_link(id, handler, radiations \\ []) do
+    GenServer.start_link(Cell, [id, handler, radiations])
+  end
 
   def init([id, handler, radiations]) do
     Logger.info("New cell #{inspect id} initialization with #{inspect handler}. Initial radiations: #{inspect radiations}")
@@ -29,38 +29,37 @@ defmodule Aether.Cell do
       pid ->
         GenServer.cast(pid, {:wave, from, wave})
     end
-    :ok
   end
 
-	defp schedule_radiation(from, radiations) do
-		Logger.info("Schedule radiations #{inspect radiations} from #{inspect from}")
-		for r <- radiations do
-			:timer.apply_after(r.after || 0, 
-												 :erlang, :apply,
-												 [&radiate_wave/3, [from, r.to, r.wave]])
-		end
-		:ok
-	end
+  defp schedule_radiation(from, radiations) do
+    Logger.info("Schedule radiations #{inspect radiations} from #{inspect from}")
+    Enum.each radiations, fn r ->
+      :timer.apply_after(r.after || 0, :erlang, :apply,
+        [&radiate_wave/3, [from, r.to, r.wave]])
+    end
+  end
 
-	def handle_call(_, _from, state) do
-		{:noreply, state}
-	end
+  def handle_call(_, _from, state) do
+    {:noreply, state}
+  end
 
   def handle_cast({:wave, from, wave}, state) do
     Logger.info("Received wave #{inspect wave} from #{inspect from}.")
     {handler, radiations} = state.handler.(from, wave)
-    if handler, do: state = %Cell{state | handler: handler}
+    if handler do
+      state = %Cell{state | handler: handler}
+    end
     schedule_radiation(state.id, radiations)
     {:noreply, state}
   end
 
-	def handle_cast(any, state) do
-		Logger.warn("Unknown cast message: #{inspect any}")
-		{:noreply, state}
-	end
+  def handle_cast(any, state) do
+    Logger.warn("Unknown cast message: #{inspect any}")
+    {:noreply, state}
+  end
 
-	def handle_info(any, state) do
-		Logger.warn("Unknown info message: #{inspect any}")
-		{:noreply, state}
-	end
+  def handle_info(any, state) do
+    Logger.warn("Unknown info message: #{inspect any}")
+    {:noreply, state}
+  end
 end
