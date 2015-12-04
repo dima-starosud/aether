d8e35b3ca1b03b029e2b69ebb1f322a35b25f428
Dmytro Starosud <d.starosud@gmail.com>
2015-12-04 23:13:30 +0200

    supervising two cells requries explicit ids for children

--- lib/aether/cell/supervisor.ex
+++ lib/aether/cell/supervisor.ex
@@ -9,7 +9,7 @@ defmodule Aether.Cell.Supervisor do
 
   def init(cells) do
     cells |>
-      Enum.map(&worker(Cell, &1)) |>
+      Enum.map(&worker(Cell, &1, id: hd(&1))) |>
       supervise(strategy: :one_for_one)
   end
 end
--- test/aether_test/cell_supervisor_test.exs
+++ test/aether_test/cell_supervisor_test.exs
@@ -10,4 +10,18 @@ defmodule AetherTest.CellSupervisorTest do
     assert_receive {:radiation, ^id, ^id, ^wave}
     assert_receive {:radiation, ^id, ^id, ^wave}
   end
+
+  test "supervise two cells" do
+    handler = fn _, _ -> exit(:boom) end
+    Aether.Cell.subscribe(:c1)
+    Aether.Cell.subscribe(:c2)
+    Aether.Cell.Supervisor.start_link([
+      [:c1, handler, [%Aether.Radiate{to: :c2, wave: nil}]],
+      [:c2, handler, [%Aether.Radiate{to: :c1, wave: nil}]]
+    ])
+    assert_receive {:radiation, :c1, :c2, nil}
+    assert_receive {:radiation, :c2, :c1, nil}
+    assert_receive {:radiation, :c1, :c2, nil}
+    assert_receive {:radiation, :c2, :c1, nil}
+  end
 end
