b2039f14123a48a19f19d52306df2507d5ebd37f
Dmytro Starosud <d.starosud@gmail.com>
2015-12-16 20:59:54 +0200

    use 'method' call

--- examples/pixel2d/putpixel.nim
+++ examples/pixel2d/putpixel.nim
@@ -3,25 +3,24 @@ from colors import nil
 from sdl import nil
 from parseutils import nil
 import sequtils
-from strutils import nil
+from strutils import split
 
 proc parseInt(s: string): int =
   if parseutils.parseInt(s, result, 0) != len(s):
     raise newException(ValueError, "Cannot parse int from '" & s & "'.")
 
 proc splitInts(s: string): seq[int] =
-  strutils.split(s).filterIt(it != "").map(parseInt)
+  s.split.filterIt(it != "").map(parseInt)
 
 let
-  nums = splitInts(system.readLine(system.stdin))
+  nums = stdin.readLine.splitInts
   ss = newScreenSurface(nums[0], nums[1])
 
-for line in system.lines(system.stdin):
+for line in stdin.lines:
   let
-    data = splitInts(line)
+    data = line.splitInts
     x = data[0]
     y = data[1]
     c = colors.Color(data[2])
   ss[x, y] = c
-  #sdl.updateRect(ss.s, int32(x), int32(y), 1, 1) ???
   sdl.updateRect(ss.s, 0, 0, 0, 0)
