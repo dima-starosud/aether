c0d1fdee502830f6113f287d14b354f6a48b79b8
Dmytro Starosud <d.starosud@gmail.com>
2018-07-14 15:33:00 +0300

    test

--- scripts/simple_planar.exs
+++ scripts/simple_planar.exs
@@ -4,6 +4,7 @@ defmodule Planar.Simple do
 
   alias Aether.Radiate
 
+  @zuzuz """
   @x 160
   @y 120
   @x0 div(@x, 2)
@@ -12,91 +13,102 @@ defmodule Planar.Simple do
 
   @start_timeout 10000
 
-  defmodule Light do
-    defstruct [:pos, :direct, :zoom, :color, :src]
 
-    def create({x0, y0}, {x1, y1}, color) do
+  defmodule Ray do
+    defstruct [:pos, :direct]
+
+    def new({x0, y0}, {x1, y1}) do
       dx = x1 - x0
       dy = y1 - y0
-      length = round(:math.sqrt(dx * dx + dy * dy))
-      x0 = x0 * length
-      y0 = y0 * length
-      %Light{pos: {x0, y0}, direct: {dx, dy}, zoom: length, color: color}
+      length = :math.sqrt(dx * dx + dy * dy)
+      dx = dx / length
+      dy = dy / length
+      %Ray{pos: {x0, y0}, direct: {dx, dy}}
     end
 
-    def current(%Light{pos: {x, y}, zoom: z}) do
-      {div(x, z), div(y, z)}
+    def current(%Ray{pos: {x, y}}) do
+      {round(x), round(y)}
     end
 
-    def color(%Light{color: color}), do: color
-
-    def move(%Light{pos: {x, y}, direct: {dx, dy}} = l) do
-      l = %Light{l | pos: {x + dx, y + dy}, src: current(l)}
-      {current(l), l}
+    def move(%Ray{pos: {x, y}, direct: {dx, dy}} = r) do
+      r = %Ray{l | pos: {x + dx, y + dy}}
+      {current(r), r}
     end
 
-    def mirror(%Light{direct: {dx, dy}} = l, mirror) do
+    def mirror(%Ray{direct: {dx, dy}} = r, mirror) do
       case mirror do
         :horizontal ->
           dy = -dy
         :vertical ->
           dx = -dx
+        {n_dx, n_dy} ->
+          Boo.zoo()
       end
-      %Light{l | direct: {dx, dy}}
+      %Ray{r | direct: {dx, dy}}
     end
   end
+"""
+
+
+  defmodule Wall do
+    defstruct [:normal]
+  end
+
+
+  defmodule Pebble do
+    defstruct v: nil, m: 1, e: {0, 0}
+  end
+
+
+  defmodule Collider do
+    defstruct pebbles: %{}
+
+    defmacro remove(key) do
+      quote do
+        {:remove, unqoute(key)}
+      end
+    end
+  end
+
+
+  def handle(id, %Collider{} = c, %Pebble{} = p) do
+    {c, key} = Collider.put(c, p)
+    {c, [%Radiate{to: id, radiation: Collider.remove(key), after: @COLLISION_TIME}]}
+  end
+
+  def handle(_, %Collider{} = c, Collider.remove(key)) do
+    {c, p} = Collider.remove(c, key)
+    {p, to} = Pebble.move(p)
+    {c, [%Radiate{to: to, radiation: p, after: @CAST_TIME}]}
+  end
+
+  def handle(_, %Wall{normal: normal}, %Pebble{v: v} = p) do
+    p = Map.update!(p, :v, &(Mirror.reflect(&1, normal)))
+    {p, to} = Pebble.move(p)
+    {c, [%Radiate{to: to, radiation: p, after: @CAST_TIME}]}
+  end
+
 
   def data(id) do
     case id do
       {x, y} when x in [0, @x] or y in [0, @y] ->
-        mirror(id)
+        %Wall{normal: Not.implemented()}
       {_, _} ->
-        nil
+        %Collider{}
     end
   end
 
-  defp mirror(id) do
-    case id do
-      corner when corner in [{0, 0}, {0, @y}, {@x, 0}, {@x, @y}] ->
-        :reverse_mirror
-      {x, _} when x in [0, @x] ->
-        :vertical_mirror
-      {_, y} when y in [0, @y] ->
-        :horizontal_mirror
-    end
-  end
 
-  def initial_light() do
+  def throw_pebbles() do
     for dx <- -5..5, dy <- [-1, 1] do
       dx = 1000 * dx
       dy = 1000 * dy
       to = {@x0 + dx, @y0 + dy}
-      color = 0xFFFFFF
       {to, radiation} = Light.create({@x0, @y0}, to, color) |> Light.move()
       Aether.Cell.radiate(to, radiation)
     end
   end
 
-  def handle({x1, _}, :vertical_mirror, %Light{src: {x2, _}} = r) when x1 != x2 do
-    r |> Light.mirror(:vertical) |> move_light()
-  end
-
-  def handle({_, y1}, :horizontal_mirror, %Light{src: {_, y2}} = r) when y1 != y2 do
-    r |> Light.mirror(:horizontal) |> move_light()
-  end
-
-  def handle(_, :reverse_mirror, %Light{} = r) do
-    r |> Light.mirror(:vertical) |> Light.mirror(:horizontal) |> move_light()
-  end
-
-  def handle(_, nil, %Light{} = r) do
-    move_light(r)
-  end
-
-  def move_light(radiation) do
-    {to, radiation} = Light.move(radiation)
-    [%Radiate{to: to, radiation: radiation, after: 5}]
-  end
 
   def genIds do
     for x <- 0..@x, y <- 0..@y do
